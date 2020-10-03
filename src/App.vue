b69bbb4d603fffb88af2a308a264e88667ec05da
saar cohen <saar.cohenn@gmail.com>
2020-10-03 15:24:49 +0300

    First Vue App - a Todo App

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,100 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Vue 3 Todo app</h1>
+    <form @submit.prevent="addNewTodo">
+      <label>New Todo</label>
+      <input v-model="newTodo" name="newTodo" />
+      <button>Add New Todo</button>
+    </form>
+    <button @click="markAllDone()">Mark All Done</button>
+    <button @click="removeAllTodos()">Remove All Todos</button>
+    <ul>
+      <li v-for="(todo, index) in todos" :key="todo.id" class="todo">
+        <h3 :class="{ done: todo.done }" @click="toggleDone(todo)">
+          {{ todo.content }}
+        </h3>
+        <button @click="removeTodo(index)">Remove Todo</button>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  setup() {
+    const newTodo = ref("");
+    const todos = ref([]);
+
+    function addNewTodo() {
+      console.log(newTodo.value);
+      todos.value.push({
+        id: Date.now(),
+        done: false,
+        content: newTodo.value,
+      });
+      newTodo.value = "";
+    }
+
+    function toggleDone(todo) {
+      todo.done = !todo.done;
+    }
+
+    function removeTodo(index) {
+      todos.value.splice(index, 1);
+    }
+
+    function markAllDone() {
+      todos.value.forEach((todo) => (todo.done = true));
+    }
+
+    function removeAllTodos() {
+      todos.value = [];
+    }
+
+    return {
+      todos,
+      newTodo,
+      addNewTodo,
+      toggleDone,
+      removeTodo,
+      markAllDone,
+      removeAllTodos,
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  font-family: sans-serif;
+  padding-top: 1em;
+  padding-bottom: 1em;
+  font-size: 2em;
+  width: 80%;
+  margin: 0 auto;
+}
+input,
+textarea,
+button,
+p,
+div,
+section,
+article,
+select {
+  display: "block";
+  width: 100%;
+  font-family: sans-serif;
+  font-size: 1em;
+  margin: 0.5em;
+}
+
+.todo {
+  cursor: pointer;
+}
+
+.done {
+  text-decoration: line-through;
 }
 </style>
